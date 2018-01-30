18c58c9159b66b2fb858ec6270ed2598ce6fd2dc
Eloi Mercado <eloi.mercado1987@gmail.com>
2018-01-30 15:00:31 -0700

    add initial block test

--- test/Block-test.js
+++ test/Block-test.js
@@ -5,12 +5,46 @@ var Block = require('../lib/Block');
 
 describe('Block', function() {
   // context('should be a function', function() {
-    // Your tests here... 
+    // Your tests here...
+  var block;
 
-  it('should be a function', function() {
-    assert.isFunction(Wizard);
+  beforeEach(function() {
+    block = new Block(250, 250, 'sienna', 10, 10);
   });
 
+  it('should return true', function() {
+    assert.equal(true, true);
+  });
+
+  it('should take arrgument for x', function() {
+    const expected = 250;
+    const actual = block.x;
+    assert.equal(actual, expected);
+  });
+    
+  it('should take arrgument for y', function() {
+    const expected = 250;
+    const actual =  block.y;
+    assert.equal(actual, expected);
+  });
+
+  it('should take arrgument for color', function() {
+    const expected = 'sienna';
+    const actual = block.color;
+    assert.equal(actual, expected);
+  });
+    
+  it('should take arrugment for width', function() {
+    const expected = 10;
+    const actual = block.width;
+    assert.equal(actual, expected);
+  });
+
+  it('should take arrgument for height', function() {
+    const expected = 10;
+    const actual = block.height;
+    assert.equal(actual, expected);
+  })
 
 
 });  
