e0fb88b3d0745d33dd3a5d83c44eb79ce3f97119
Eloi Mercado <eloi.mercado1987@gmail.com>
2018-01-31 15:21:32 -0700

    correct snake length block x and y coord

--- test/Block-test.js
+++ test/Block-test.js
@@ -3,45 +3,45 @@ var assert = chai.assert;
 
 var Block = require('../lib/Block');
 
-describe('Block', function() {
+describe('Block', () => {
   // context('should be a function', function() {
     // Your tests here...
   var block;
 
 
-  beforeEach(function() {
+  beforeEach(() => {
     block = new Block(250, 250, 'sienna', 10, 10);
   });
 
-  it('should return true', function() {
+  it('should return true', () => {
     assert.equal(true, true);
   });
 
-  it('should take arrgument for x', function() {
+  it('should take arrgument for x', () => {
     const expected = 250;
     const actual = block.x;
     assert.equal(actual, expected);
   });
     
-  it('should take arrgument for y', function() {
+  it('should take arrgument for y', () => {
     const expected = 250;
     const actual =  block.y;
     assert.equal(actual, expected);
   });
 
-  it('should take arrgument for color', function() {
+  it('should take arrgument for color', () => {
     const expected = 'sienna';
     const actual = block.color;
     assert.equal(actual, expected);
   });
     
-  it('should take arrugment for width', function() {
+  it('should take arrugment for width', () => {
     const expected = 10;
     const actual = block.width;
     assert.equal(actual, expected);
   });
 
-  it('should take arrgument for height', function() {
+  it('should take arrgument for height', () => {
     const expected = 10;
     const actual = block.height;
     assert.equal(actual, expected);
